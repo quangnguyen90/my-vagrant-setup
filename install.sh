fe9f91dfac03ba36f0ebd884f4c38c478d601b65
Bui Van Hien <hienbv@rikkeisoft.com>
2017-11-09 09:29:37 +0700

    Install apache2, nodejs for ubuntu 1604, ubuntu1404

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# ******************************************
+# Developer: HienBV(Bui Van Hien)
+# Email:hienbv.it@gmail.com
+# Date: 08-11-2017
+# ******************************************
+
+OS="ubuntu1604"
+SETUP_SCRIPT=/scripts
+. $SETUP_SCRIPT/$OS/00-beforeSetup.sh
+#. $SETUP_SCRIPT/$OS/01-common.sh
+#. $SETUP_SCRIPT/$OS/02-setup-apache2.sh
+. $SETUP_SCRIPT/$OS/08-setup-nodejs-8x.sh
---
+++ scripts/ubuntu1404/00-beforeSetup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# ******************************************
+# Developer: HienBV(Bui Van Hien)
+# Email:hienbv.it@gmail.com
+# Date: 08-11-2017
+# ******************************************
+
+# Update package
+sudo apt-get update;
+# sudo apt upgrade -y;
+sudo apt-get autoremove;
+
+echo "00-beforeSetup.sh: Done!!!!!";
