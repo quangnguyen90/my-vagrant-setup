762649e7fc7b17bbe6dee18041d02cc2a878eadd
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-05-12 18:56:49 +0700

    update install nodejs 10x

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,8 +3,10 @@
 # include parse_yaml function
 . $SETUP_SCRIPT/parse_yaml.sh
 
-OS="$config_os_name"
-SETUP_SCRIPT="$config_os_setup_path"
+#OS="$config_os_name"
+#SETUP_SCRIPT="$config_os_setup_path"
+OS="ubuntu1604"
+SETUP_SCRIPT="/scripts"
 
 # SETUP
 . $SETUP_SCRIPT/$OS/00-beforeSetup.sh
@@ -14,4 +16,4 @@ SETUP_SCRIPT="$config_os_setup_path"
 . $SETUP_SCRIPT/$OS/04-setup-mysql57.sh
 . $SETUP_SCRIPT/$OS/05-setup-phpmyadmin.sh
 . $SETUP_SCRIPT/$OS/06-sites.sh
-. $SETUP_SCRIPT/$OS/08-setup-nodejs-8x.sh
+. $SETUP_SCRIPT/$OS/08-setup-nodejs-10x.sh
